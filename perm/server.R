989243d436466c93941e795d7147b3da7b7f8fbd
shyamalapriya <shyamalapriya@gmail.com>
2015-03-25 05:25:27 -0700

    Initial Checkin

---
+++ perm/server.R
@@ -0,0 +1,91 @@
+library(shiny)
+
+perm_data <- read.csv("./data/PERM_FY14_Q4.csv", header=T, stringsAsFactors=T)
+show_vars <- c("PW_Job_Title_9089", "Employer_Name", "PW_AMOUNT_9089")
+
+# Core wrapping function
+wrap.it <- function(x, len)
+{ 
+  sapply(x, function(y) paste(strwrap(y, len), 
+                              collapse = "\n"), 
+         USE.NAMES = FALSE)
+}
+
+
+# Call this function with a list or vector
+wrap.labels <- function(x, len)
+{
+  if (is.list(x))
+  {
+    lapply(x, wrap.it, len)
+  } else {
+    wrap.it(x, len)
+  }
+}
+
+shinyServer(function(input,output){
+  
+  outVarSector <- reactive({
+    vars <- as.list(levels(perm_data$US_ECONOMIC_SECTOR))
+    return(vars)
+  })
+  
+  outVarState <- reactive({
+    vars <- as.list(levels(perm_data$Job_Info_Work_State))
+    return(vars)
+  })
+  
+  output$sector = renderUI({
+    selectInput('sector', 'Sector', outVarSector())
+  })
+  
+  output$state = renderUI({
+    selectInput('state', 'State', outVarState())
+  })
+  
+  output$text1 <- renderText({
+      paste("You have selected", input$sector)
+  })
+  
+  filteredData<-reactive({
+    if(!(is.null(input$sector) || input$sector=="")) 
+        return(perm_data[which(perm_data$US_ECONOMIC_SECTOR==input$sector),])
+     else 
+       return(perm_data)
+    })
+  
+  filteredStateData<-reactive({
+    if(!(is.null(input$state) || input$state=="")) 
+      return(filteredData()[which(filteredData()$Job_Info_Work_State==input$state),])
+    else
+      return(filteredData())
+  })
+  
+  aggr_dataset <- reactive({aggregate(filteredStateData()[["PW_AMOUNT_9089"]],
+                                      by=list(filteredStateData()[["PW_Job_Title_9089"]]), 
+                                     FUN=mean,
+                                     na.rm=T)})
+  
+  sort1.aggr_dataset<-reactive({aggr_dataset()[with(aggr_dataset(),
+                                                   order(x, decreasing = T, na.last=T)),]})
+  
+  output$salaryPlot <- renderPlot({
+          barplot(sort1.aggr_dataset()[1:10,2],
+            names.arg=wrap.labels(sort1.aggr_dataset()[1:10,1],15),
+            xlab="Job Title",
+            ylab="Median Salary",
+            ylim=c(0,250000),
+            main="Pay Scale",
+            border="black")
+  })
+  
+  output$summary <- renderTable({
+    sort1.aggr_dataset()
+  })
+  
+  output$salaryTable<-renderDataTable({
+    library(ggplot2)
+    #diamonds[, input$show_vars, drop = FALSE]
+    filteredStateData()[,show_vars, drop=FALSE]
+    })
+})
---
+++ perm/ui.R
@@ -0,0 +1,44 @@
+library(shiny)
+library(ggplot2)
+library(shinydashboard)
+
+# Define UI for Top Paying jobs application
+shinyUI(
+  
+  dashboardPage(
+    dashboardHeader(title = "Foreign worker data"),
+    dashboardSidebar(
+      sidebarMenu(
+        menuItem("Salary by Title", tabName = "title"),
+        menuItem("Salary by Company", tabName = "company")
+      )
+      ),
+    dashboardBody(
+      tabItems(
+        # First tab content
+        tabItem(tabName = "title",
+          fluidPage(
+                fluidRow(
+                    column(12,
+                          fluidRow(
+                              column(2,
+                              h4("Select Inputs"),
+                              uiOutput('sector'),
+                              uiOutput('state')
+                          ),
+                    column(10,
+                          h4("Pay Scale"),
+                          plotOutput("salaryPlot")
+                   )
+                 ),
+                 fluidRow(
+                   column(12,
+                          h4("Data Table"),
+                          dataTableOutput("salaryTable")))
+                 )
+            ))),
+        # Second tab content
+        tabItem(tabName = "company",
+                h2("Company tab content")
+        )
+))))
